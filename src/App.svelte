bcc97b0f444434b0021ab8904cbc53ce4222d949
kvssankar <kvs.sankar001@gmail.com>
2021-03-09 17:38:18 +0530

    working

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Router from "svelte-spa-router";
+  import Home from "./pages/Home.svelte";
+  import Login from "./pages/Login.svelte";
+  import ResetPassword from "./pages/ResetPassword.svelte";
+
+  const routes = {
+    "/": Home,
+
+    "/login": Login,
+
+    // '*': NotFound,
+  };
+</script>
+
+<Router {routes} />
---
+++ src/pages/Login.svelte
@@ -0,0 +1,91 @@
+<script>
+  import { link } from "svelte-spa-router";
+</script>
+
+<body
+  data-theme="default"
+  data-layout="fluid"
+  data-sidebar-position="left"
+  data-sidebar-behavior="sticky"
+>
+  <div class="main d-flex justify-content-center w-100">
+    <main class="content d-flex p-0">
+      <div class="container d-flex flex-column">
+        <div class="row h-100">
+          <div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
+            <div class="d-table-cell align-middle">
+              <div class="text-center mt-4">
+                <h1 class="h2">Welcome back, Chris</h1>
+                <p class="lead">Sign in to your account to continue</p>
+              </div>
+
+              <div class="card">
+                <div class="card-body">
+                  <div class="m-sm-4">
+                    <div class="text-center">
+                      <img
+                        src="img/avatars/avatar.jpg"
+                        alt="Chris Wood"
+                        class="img-fluid rounded-circle"
+                        width="132"
+                        height="132"
+                      />
+                    </div>
+                    <form>
+                      <div class="form-group">
+                        <label for="">Email</label>
+                        <input
+                          class="form-control form-control-lg"
+                          type="email"
+                          name="email"
+                          placeholder="Enter your email"
+                        />
+                      </div>
+                      <div class="form-group">
+                        <label for="">Password</label>
+                        <input
+                          class="form-control form-control-lg"
+                          type="password"
+                          name="password"
+                          placeholder="Enter your password"
+                        />
+                        <small>
+                          <a use:link href="pages-reset-password.html"
+                            >Forgot password?</a
+                          >
+                        </small>
+                      </div>
+                      <div>
+                        <div
+                          class="custom-control custom-checkbox align-items-center"
+                        >
+                          <input
+                            type="checkbox"
+                            class="custom-control-input"
+                            value="remember-me"
+                            name="remember-me"
+                            checked
+                          />
+                          <label for="" class="custom-control-label text-small"
+                            >Remember me next time</label
+                          >
+                        </div>
+                      </div>
+                      <div class="text-center mt-3">
+                        <a
+                          href="dashboard-default.html"
+                          class="btn btn-lg btn-primary">Sign in</a
+                        >
+                        <!-- <button type="submit" class="btn btn-lg btn-primary">Sign in</button> -->
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</body>
